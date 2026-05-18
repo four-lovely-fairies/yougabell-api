b09913ae203cc42f9201de84aa0bb7698445d0a0
jojo <pch468@naver.com>
2026-05-18 18:06:18 +0900

    feat(api): 미션 시작과 타이머 플로우 추가

---
+++ missions/mission-flow.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { CurrentUserId } from '../auth/current-user.decorator';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import {
+  GetActiveMissionExecutionQueryDto,
+  GetCurrentMissionQueryDto,
+  GetCurrentMissionResponseDto,
+  MissionExecutionActionBodyDto,
+  MissionExecutionSnapshotResponseDto,
+  StartMissionExecutionDto,
+} from './dto/mission-flow.dto';
+import { MissionsService } from './missions.service';
+
+@ApiTags('missions')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@Controller()
+export class MissionFlowController {
+  constructor(private readonly missionsService: MissionsService) {}
+
+  @Get('missions/current')
+  @ApiOkResponse({ type: GetCurrentMissionResponseDto })
+  getCurrentMission(
+    @CurrentUserId() userId: string,
+    @Query() query: GetCurrentMissionQueryDto,
+  ) {
+    return this.missionsService.getCurrentMission(userId, query);
+  }
+
+  @Post('mission-executions')
+  @ApiOkResponse({ type: MissionExecutionSnapshotResponseDto })
+  startMissionExecution(
+    @CurrentUserId() userId: string,
+    @Body() body: StartMissionExecutionDto,
+  ) {
+    return this.missionsService.startMissionExecution(userId, body);
+  }
+
+  @Get('mission-executions/active')
+  @ApiOkResponse({ type: MissionExecutionSnapshotResponseDto })
+  getActiveMissionExecution(
+    @CurrentUserId() userId: string,
+    @Query() query: GetActiveMissionExecutionQueryDto,
+  ) {
+    return this.missionsService.getActiveMissionExecution(userId, query);
+  }
+
+  @Post('mission-executions/:id/action')
+  @ApiOkResponse({ type: MissionExecutionSnapshotResponseDto })
+  applyMissionExecutionAction(
+    @CurrentUserId() userId: string,
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: MissionExecutionActionBodyDto,
+  ) {
+    return this.missionsService.applyMissionExecutionAction(
+      userId,
+      id,
+      body.action,
+    );
+  }
+}
--- missions/missions.module.ts
+++ missions/missions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { MissionFlowController } from './mission-flow.controller';
 import { MissionsController } from './missions.controller';
 import { MissionsService } from './missions.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [MissionsController],
+  controllers: [MissionsController, MissionFlowController],
   providers: [MissionsService],
 })
 export class MissionsModule {}
